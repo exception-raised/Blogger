8772fcc0a544348fd7db36a74942038d7910af4e
exception-raised <dimmelman@riseup.net>
2024-10-07 11:25:11 +0200

    Server prototype

---
+++ server/database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE bloggerdb;
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "username" varchar,
+  "password" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "blogs" (
+  "blog_id" SERIAL PRIMARY KEY,
+  "title" varchar,
+  "likes" integer,
+  "favorites" integer,
+  "user_id" integer
+);
