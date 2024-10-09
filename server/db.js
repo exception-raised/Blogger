12b1d89a7cc408e2fb1aef145452b8b7c300b8f8
exception-raised <dimmelman@riseup.net>
2024-10-09 15:40:08 +0200

    Dockerfile

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "",
-    host: "localhost",
-    port: 5432,
-    database: "bloggerdb"
-});
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
 
 module.exports = pool;
